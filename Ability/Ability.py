3bf42195defd455a1d4354acbff6616652b7d836
AlexPagarsky <alex.pagarski@gmail.com>
2019-11-07 22:33:11 +0200

    Attack exception trigger remade

--- Ability/Ability.py
+++ Ability/Ability.py
@@ -1,5 +1,5 @@
 import Unit
-
+from exceptions import *
 
 class Ability:
 
@@ -7,10 +7,16 @@ class Ability:
         self.owner = owner
 
     def attack(self, enemy : Unit):
-        if enemy.is_alive() and enemy != self:
+        if enemy is self.owner:
+            raise AttacksItself()
+        if not self.owner.is_alive():
+            raise CantDoCauseDead()
+        if enemy.is_alive():
             enemy.take_damage(self.owner.dmg)
             enemy.counter_attack(self.owner)
 
     def counter_attack(self, enemy : Unit):
-        if self.owner.is_alive() and enemy.is_alive():
+        if not self.owner.is_alive():
+            raise CantDoCauseDead()
+        if enemy.is_alive():
             enemy.take_damage(self.owner.dmg // 2)
