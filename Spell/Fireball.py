27cecce817e84f2158f950d3e28a97e7687e8bcf
AlexPagarsky <alex.pagarski@gmail.com>
2019-11-08 21:41:49 +0200

    More tests done

--- Spell/Fireball.py
+++ Spell/Fireball.py
@@ -10,7 +10,4 @@ class Fireball(AbstractSpell):
         AbstractSpell.__init__(self, dmg, cost)
 
     def damage(self, target):
-        if target.__class__.__name__ == "Werewolf":
-            target.take_magic_damage(self.dmg*2)
-        else:
-            target.take_magic_damage(self.dmg)
+        target.take_magic_damage(self.dmg)
--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
 from Unit.Soldier import Soldier
 from Unit.Rogue import Rogue
 from Unit.Berserker import Berserker
-from Unit.Werewolf import Werewolf
 from Unit.Vampire import Vampire
+from Unit.Werewolf import Werewolf
+
+from SpellCaster.Wizard import Wizard
+from SpellCaster.Healer import Healer
+from SpellCaster.Necromancer import Necromancer
+from SpellCaster.Warlock import Warlock
+from SpellCaster.Priest import Priest
+
 from config import *
 import exceptions
 import unittest
@@ -46,6 +53,71 @@ class TestRogue(unittest.TestCase):
         sold = Soldier.Soldier()
         sold.attack(rog)
         self.assertLess(rog.hp, Hp.ROGUE_HP.value)
+
+
+class TestBerserker(unittest.TestCase):
+
+    def test_turnable(self):
+        ber = Berserker.Berserker()
+        self.assertEqual(ber.turnable, True)
+
+    def test_atk(self):
+        ber = Berserker.Berserker()
+        sold = Soldier.Soldier()
+        sold.attack(ber)
+        self.assertLess(ber.hp, ber.hp_limit)
+
+    def test_magattk(self):
+        ber = Berserker.Berserker()
+        wiz = Wizard.Wizard()
+        wiz.cast_spell("Fireball", ber)
+        self.assertEqual(ber.hp, ber.hp_limit)
+
+
+class TestVampire(unittest.TestCase):
+
+    def test_turnable(self):
+        vamp = Vampire.Vampire()
+        self.assertEqual(vamp.turnable, False)
+
+    def test_lifesteal(self):
+        vamp = Vampire.Vampire()
+        sold = Soldier.Soldier()
+        sold.attack(vamp)
+        sold.attack(vamp)
+        self.assertEqual(vamp.hp, 55)
+
+
+class TestWerewolf(unittest.TestCase):
+
+    def test_turnable(self):
+        woof = Werewolf.Werewolf()
+        self.assertEqual(woof.turnable, False)
+
+    def test_turn(self):
+        woof = Werewolf.Werewolf()
+        woof.turn()
+        self.assertEqual(woof.hp_limit, Hp.WEREWOLF_BASE_HP.value+50)
+        self.assertEqual(woof.dmg, Dmg.WEREWOLF_BASE_DMG.value*2)
+
+    def test_magattk(self):
+        woof = Werewolf.Werewolf()
+        wiz = Wizard.Wizard()
+        woof.turn()
+        wiz.cast_spell("Fireball", woof)
+        self.assertEqual(woof.hp, 75)
+
+    def test_except(self):
+        woof = Werewolf.Werewolf()
+        wof2 = Werewolf.Werewolf()
+        sold = Soldier.Soldier()
+        woof.take_damage(1000000)
+        with self.assertRaises(exceptions.CantDoCauseDead):
+            woof.attack(sold)
+        with self.assertRaises(exceptions.TargetIsDead):
+            wof2.attack(woof)
+        with self.assertRaises(exceptions.CantTurn):
+            wof2.turn(wof2)
 
 
 if __name__ == "__main__":
